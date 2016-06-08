83560569cb588e2dbdd4a930a8ba358bc9364e7b
ttff521xx <ttff521xx@126.com>
2016-06-08 09:56:52 +0800

    22

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyGitHubProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("您好！");
+            Console.WriteLine("您好！");
+            Console.WriteLine("您好！");
+            Console.WriteLine("您好！");
+            Console.ReadKey();
+        }
+    }
+}
